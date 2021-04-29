12bea91729194058417ea5027819d8ad1f72d39d
jlmartinezdev <jose_mart289@live.com>
2021-04-29 11:16:34 -0400

    Fecha de caducidad

--- app/src/main/java/com/jlmartinez/votos2021/MainActivity.kt
+++ app/src/main/java/com/jlmartinez/votos2021/MainActivity.kt
@@ -4,22 +4,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main)
         var comenzar= findViewById<Button>(R.id.buttoncomenzar)
         comenzar.setOnClickListener{
-//            val intento1 = Intent(this, ventana2::class.java)
-//            startActivity(intento1)
+
             val intent = Intent(this, ventana2::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivityForResult(intent, 0)
             overridePendingTransition(0, 0)
             finish()
         }
-        // this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        val expirationDate: Calendar = Calendar.getInstance()
+        expirationDate.set(2021, 7, 15) //expirara final de año
+
+        val t: Calendar = Calendar.getInstance() //obtener fecha actual
+
+        if (t.compareTo(expirationDate) === 1) finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val expirationDate: Calendar = Calendar.getInstance()
+        expirationDate.set(2021, 7, 15) //expirara final de año
+
+        val t: Calendar = Calendar.getInstance() //obtener fecha actual
+
+        if (t.compareTo(expirationDate) === 1) finish()
     }
 }
